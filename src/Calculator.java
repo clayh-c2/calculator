33ab700787cb65a0f5da244da55a6a094faf2f73
Clay Huang <clayhzc2@gmail.com>
2025-08-29 15:14:23 -0400

    Added colors and initial display panel

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,14 @@ public class Calculator {
     int boardWidth = 360;
     int boardHeight = 540;
 
+    Color lightGray = new Color(212, 212, 210);
+    Color darkGray = new Color(80, 80, 80);
+    Color black = new Color(28, 28, 28);
+    Color orange = new Color(255, 149, 0);
+
     JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
 
     Calculator() {
         // Creates inital JFrame
@@ -18,6 +25,17 @@ public class Calculator {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(black);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel, BorderLayout.NORTH);
     }
 
 }
